a53b4bfb5ccf809696c4e3355ee1cebf9defc6ac
IsVivian <384120230@qq.com>
2016-05-03 16:21:28 +0800

    Initial Commit

---
+++ kiwidemoTests/kiwidemoTests.m
@@ -0,0 +1,39 @@
+//
+//  kiwidemoTests.m
+//  kiwidemoTests
+//
+//  Created by sherry on 16/5/3.
+//  Copyright © 2016年 sherry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kiwidemoTests : XCTestCase
+
+@end
+
+@implementation kiwidemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
